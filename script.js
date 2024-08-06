8c2c9c1ee77ef2baf7c29ec1ff557508897a408b
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-08-06 16:42:08 -0600

    Created 3 dummy variables (operator 2 num) that use button.id for now
    Created function operate which takes the operator
    and 2 numbers and calls a operator function

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b
 }
+
+function operate(operator,number1,number2) {
+    if (operator == "add") {
+        return add(number1,number2)
+    }
+    else if (operator == "subtract") {
+        return subtract(number1,number2)
+    }
+    else if (operator == "multiply") {
+        return multiply(number1,number2)
+    }
+    else if (operator == "divide") {
+        return divide(number1,number2)
+    }
+}
+
+let number1 = button.id;
+let number2 = button.id;
+let operator = button.id;
