3b507b3ab92464b009f29531dff7d61157ad5850
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-08-09 16:46:41 -0600

    Added functionality to operator buttons     has display and changes the operator Equals button will perform and display the operation     Edited the operator ids to perform the correct operation Clear button will clear all values and display becomes 0

--- script.js
+++ script.js
@@ -38,11 +38,28 @@ let number1 = 0;
 let number2 = 0;
 let operator = "";
 let userInput = "";
+const numericalValues = "1234567890";
 const buttons = document.querySelectorAll("button")
 const displayValue = document.querySelector(".displayText")
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         userInput = button.id
-        displayValue.textContent = Number(userInput);
+        if (button.id == "clear") {
+            number1 = 0;
+            number2 = 0;
+            operator = "";
+            userInput = "";
+            displayValue.textContent = "0";
+        }
+        else if (numericalValues.includes(button.id)) {
+            displayValue.textContent = button.id;
+        }
+        else if (button.classList.contains("operator")) {
+            displayValue.textContent = button.innerText;
+            operator = button.id;
+        }
+        else if (button.id == "equals") {
+            displayValue.textContent = operate(operator,number1,number2)
+        }
     })
 })
