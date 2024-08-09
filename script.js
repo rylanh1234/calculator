2757975440c6d5d9624bab5ea075e13bf3725ca6
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-08-09 16:13:43 -0600

    Clicking number buttons populates the display, but it only does 1 number for now
    Edited divide function to return "ERROR" if dividing by 0

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
-function add(a,b) {
+function add(a, b) {
     return a + b
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
     return a * b
 }
 
-function divide(a,b) {
-    return a / b
+function divide(a, b) {
+    if (b == 0) {
+        return "ERROR"
+    }
+    else {
+        return a / b
+    }
 }
 
-function operate(operator,number1,number2) {
+function operate(operator, number1, number2) {
     if (operator == "add") {
-        return add(number1,number2)
+        return add(number1, number2)
     }
     else if (operator == "subtract") {
-        return subtract(number1,number2)
+        return subtract(number1, number2)
     }
     else if (operator == "multiply") {
-        return multiply(number1,number2)
+        return multiply(number1, number2)
     }
     else if (operator == "divide") {
-        return divide(number1,number2)
+        return divide(number1, number2)
     }
 }
 
-let number1 = button.id;
-let number2 = button.id;
-let operator = button.id;
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+let userInput = "";
+const buttons = document.querySelectorAll("button")
+const displayValue = document.querySelector(".displayText")
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        userInput = button.id
+        displayValue.textContent = Number(userInput);
+    })
+})
